8acf4b5c7ff63b57446cab5948a844327822b2d9
Andy Dziabo <jameszabo@yahoo.com>
2022-08-14 22:10:09 -0400

    commit tree 080ccc122a2c1b2f0dfca862e433be8a3f6d1b13

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+// const form = document.getElementById('userForm');
+// form.addEventListener('submit', submitData);
+
+function submitData(name, email){
+    //console.log(name);
+    //console.log(email);
+
+    let obj = {
+        name,
+        email,
+    }
+    //console.log(obj);
+    // e.preventDefault();
+    // const user = {
+    //     name: document.getElementById('name'),
+    //     email: document.getElementById('email'),
+    // };
+    // console.log(user.name.value);
+    // console.log(user.email.value);
+    const element = document.createElement('h1');
+    
+    return fetch('http://localhost:3000/users', {
+         method: 'POST',
+         headers: {
+             'Content-Type': 'application/json',
+             'Accept': 'application/json',
+         },
+         body: JSON.stringify(obj)
+    })
+    .then(res => res.json())
+    .then(data => document.body.append(element.textContent = data.id))
+    .catch(error => document.body.append(element.textContent = error))    
+}
+submitData("dave","email");
